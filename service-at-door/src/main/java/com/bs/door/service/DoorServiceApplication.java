388137075bbb697f8ba8a811d2e5beffb608b858
bharatvr <bharat.vr@gmail.com>
2018-08-27 20:33:48 -0400

    Coce cleanup

---
+++ service-at-door/src/main/java/com/bs/door/service/DoorServiceApplication.java
@@ -0,0 +1,16 @@
+package com.bs.door.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author bharat
+ *
+ */
+@SpringBootApplication
+public class DoorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoorServiceApplication.class, args);
+	}
+}
